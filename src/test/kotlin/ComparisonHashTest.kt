1b9224382e7374901938b6ce5795fcf202a9b847
hermanest <nicedevelooper@gmail.com>
2024-12-16 00:34:08 +0300

    Fixed tests not being able to compile.

--- src/test/kotlin/ComparisonHashTest.kt
+++ src/test/kotlin/ComparisonHashTest.kt
@@ -5,15 +5,15 @@ import kotlin.test.assertEquals
 class ComparisonHashTest {
     @Test
     fun basic() {
-        val comparison1 = Comparison(0.0, "hello", "world", null)
-        val comparison2 = Comparison(1.0, "world", "hello", null)
+        val comparison1 = Comparison(0.0, "hello", "world")
+        val comparison2 = Comparison(1.0, "world", "hello")
         assertEquals(comparison1, comparison2)
     }
     
     @Test
     fun hashSet() {
-        val comparison1 = Comparison(0.0, "hello", "world", null)
-        val comparison2 = Comparison(1.0, "world", "hello", null)
+        val comparison1 = Comparison(0.0, "hello", "world")
+        val comparison2 = Comparison(1.0, "world", "hello")
         val set = hashSetOf<Comparison>()
         
         set.add(comparison1)

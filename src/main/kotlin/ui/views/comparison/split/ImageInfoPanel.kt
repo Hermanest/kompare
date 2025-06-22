0246972d19ba17b70008f5a7b623bf7523782346
hermanest <nicedevelooper@gmail.com>
2025-06-22 18:53:53 +0300

    Added filename to image info panel.

--- src/main/kotlin/ui/views/comparison/split/ImageInfoPanel.kt
+++ src/main/kotlin/ui/views/comparison/split/ImageInfoPanel.kt
@@ -1,24 +1,25 @@
 package ui.views.comparison.split
 
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import utils.clamp
 import utils.getFileSize
 import utils.showInExplorer
+import kotlin.io.path.Path
 
 @Composable
 fun ImageInfoPanel(
@@ -40,11 +41,44 @@ fun ImageInfoPanel(
             style = MaterialTheme.typography.bodySmall,
             color = MaterialTheme.colorScheme.onSurface
         )
+
         Text(
             text = "Size: $fileSize",
             style = MaterialTheme.typography.bodySmall,
             color = MaterialTheme.colorScheme.onSurface
         )
+
+        var textLayoutResult by remember { mutableStateOf<TextLayoutResult?>(null) }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .padding(top = 4.dp)
+                .clipToBounds()
+        ) {
+            val modifier = Modifier.align(Alignment.Center).run {
+                if (textLayoutResult != null) {
+                    val delta = textLayoutResult!!.multiParagraph.width - textLayoutResult!!.size.width
+
+                    this.offset(x = -delta.coerceAtLeast(0f).dp / 2f)
+                } else {
+                    this
+                }
+            }
+
+            Text(
+                text = "${Path(path).fileName}",
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Visible,
+                softWrap = false,
+                modifier = modifier,
+                onTextLayout = { textLayoutResult = it }
+            )
+        }
+
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier.padding(top = 8.dp)
